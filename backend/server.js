7dc18444c147bea4f99e72e6ac81b474dde7d5b9
Galen Casstevens <galencasstevens@gmail.com>
2023-05-08 16:16:12 -0400

    connect to mongoose, created a model, created error middleware

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,25 @@
 const express = require('express');
+const colors = require('colors');
 const dotenv = require('dotenv').config();
-const PORT = 5000;
+const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+const PORT = process.env.PORT || 5000;
+
+// Connect to database
+connectDB();
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
 	res.status(200).json({ message: 'Welcome to the Loc8r app!' });
 });
 
+// Routes
 app.use('/api/locations', require('./routes/locationRoutes'));
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
