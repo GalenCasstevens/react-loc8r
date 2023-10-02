3863d8e065b88036d80fd766276d8975a31c479a
Galen Casstevens <galencasstevens@gmail.com>
2023-10-01 21:42:18 -0400

    removed serviceWorker import

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,8 @@
+import React from 'react';
+import { ReactDOM } from 'react';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
